bc23358da1a21f3adba045a9584fdc003d2bdb76
Danyal <68073783+imRanDan@users.noreply.github.com>
2021-11-06 12:09:11 -0400

    added tweakpane and some other stuff

--- sketch-04.js
+++ sketch-04.js
@@ -1,17 +1,29 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
+const math = require('canvas-sketch-util/math');
+const Tweakpane = require('tweakpane');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
+  animate: true
+};
+
+const params = {
+  cols: 10,
+  rows: 10,
+  scaleMin: 1,
+  scaleMax: 30,
+  freq: 0.001,
+  amp: 0.2,
 };
 
 const sketch = () => {
-  return ({ context, width, height }) => {
+  return ({ context, width, height, frame }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    const cols = 10;
-    const rows = 10;
+    const cols = params.cols;
+    const rows = params.rows;
     const numCells = cols * rows;
 
     const gridw = width * 0.8;
@@ -30,11 +42,11 @@ const sketch = () => {
       const w = cellw * 0.8;
       const h = cellh * 0.8;
 
-      const n = random.noise2D(x, y, 0.001);
+      const n = random.noise2D(x + frame * 20, y, params.freq);
       const angle = n * Math.PI * 0.2;
-
       //const scale = (n + 1) / 2 * 30;
-      const scale = (n * 0.5 + 0.5) * 30;
+      //const scale = (n * 0.5 + 0.5) * 30;
+      const scale = math.mapRange(n, -1, 1, params.scaleMin, params.scaleMax);
 
       context.save();
       context.translate(x, y);
@@ -42,7 +54,7 @@ const sketch = () => {
       context.translate(cellw * 0.5, cellh * 0.5);
       context.rotate(angle);
 
-      context.lineWidth = 4;
+      context.lineWidth = scale;
 
       context.beginPath();
       context.moveTo(w * -0.5, 0);
@@ -57,4 +69,20 @@ const sketch = () => {
   };
 };
 
+const createPane = () => {
+  const pane = new Tweakpane.Pane();
+  let folder;
+
+  folder = pane.addFolder({title: 'Grid'});
+  folder.addInput(params, 'cols', { min: 2, max: 50, step: 1});
+  folder.addInput(params, 'rows', { min: 2, max: 50, step: 1});
+  folder.addInput(params, 'scaleMin', { min: 1, max: 100 });
+  folder.addInput(params, 'scaleMax', { min: 1, max: 100 });
+
+  folder = pane.addFolder({ title: 'Noise' });
+  folder.addInput(params, 'freq', { min: -0.01, max: 0.01 });
+  folder.addInput(params, 'amp', { min: 0, max: 1 });
+};
+
+createPane();
 canvasSketch(sketch, settings);
