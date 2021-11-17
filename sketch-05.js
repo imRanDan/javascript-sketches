e1cbe4109949920aa7c75de391744dadd28e4bef
Danyal <68073783+imRanDan@users.noreply.github.com>
2021-11-17 14:26:18 -0500

    more changes and stuff

--- sketch-05.js
+++ sketch-05.js
@@ -1,33 +1,71 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [ 1080, 1080 ]
 };
 
-const sketch = () => {
+let manager;
+
+let text = 'D';
+let fontSize = 1200;
+let fontFamily = 'serif';
+
+const typeCanvas = decoument.createElement('canvas');
+const typeContext = typeCanvas.getContext('2d');
+
+const sketch = ({ context, width, height }) => {
+  const cell = 20;
+  const cols = Math.floor(width / cell);
+  const rows = Math.floor(height / cell);
+  const numCells = cols * rows;
+
+  typeCanvas.width = cols;
+  typeCanvas.height = rows;
+
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+    typeContext.fillStyle = 'white';
+    typeContext.fillRect(0, 0, cols, height);
 
-    context.fillStyle = 'black';
-    context.font = '1200px serif';
-    context.textBaseline = 'middle';
-    //context.textAlign = 'center';
+    typeContext.fillStyle = 'black';
+    typeContext.font =  `${fontSize}px ${fontFamily}`;
+    typeContext.textBaseline = 'top';
 
-    const text = 'D';
 
-    const metrics = context.measureText(text);
+    const metrics = typeContext.measureText(text);
     const mx = metrics.actualBoundingBoxLeft * -1;
     const my = metrics.actualBoundingBoxAscent * -1;
     const mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
     const mh = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
 
-    context.save();
-    //context.translate(width * 0.5, height * 0.5);
+    const x = (cols - mw) * 0.5 - mx;
+    const y = (height - mh) * 0.5 - my;
 
-    context.fillText('D', 0, 0);
-    context.restore();
+    typeContext.save();
+    typeContext.translate(x, y);
+
+    typeContext.beginPath();
+    typeContext.rect(mx, my, mw, mh);
+    typeContext.stroke();
+
+    typeContext.fillText(text, 0, 0);
+    typeContext.restore();
   };
 };
 
-canvasSketch(sketch, settings);
+const onKeyUp = (e) => {
+  text = e.key.toUpperCase();
+  manager.render();
+};
+
+document.addEventListener('keyup', onKeyUp);
+
+
+const start = async () => {
+  manager = await canvasSketch(sketch, settings);
+};
+
+start();
+
+
+
+
